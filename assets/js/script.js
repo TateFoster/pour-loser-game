f0b287d332a7c4e3a4a48a55516373197e43c99e
Dragon Lore <tate.j.foster@gmail.com>
2022-08-17 15:08:34 -0600

    storing for pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,38 +9,37 @@ var answer = document.querySelector("#answer");
 var quiz;
 
 function getData(data) {
-  console.log(data);
-  var questionArray = [];
-  // for index in data
-  // data.question
+	console.log(data);
+	var questionArray = [];
+	// for index in data
+	// data.question
 }
 
 fetch(quizUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    var newData = data;
-    getData(newData);
-  });
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+		var newData = data;
+		getData(newData);
+	});
 
 // TODO: variables score, mistakes, question/answer  form
 
 fetch(rewardUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+	});
 
 // TODO: FUNction to generate questions from API
 function startQuiz(event) {
-  event.preventDefault();
-  start.classList.add("hidden");
-  answerForm.classList.remove("hidden");
+	event.preventDefault();
+	start.classList.add("hidden");
+	answerForm.classList.remove("hidden");
 }
 // TODO: FUnction to display drink recipe on loss
 
@@ -58,29 +57,29 @@ start.addEventListener("click", startQuiz);
 var keywordsArray = ["apple", "fritters", "good"];
 var userAnswerString = "apple fritters bad";
 function isCorrectAnswer(keywordsArray, userAnswerString) {
-  var thresholdToBeCorrect = 0.8; // min. required % to be correct
-  var countKeywords = keywordsArray.length; // total number of keywords in keywordsArray
-  var matchedKeywords = 0; // tracker for correctly answered keywords
+	var thresholdToBeCorrect = 0.8; // min. required % to be correct
+	var countKeywords = keywordsArray.length; // total number of keywords in keywordsArray
+	var matchedKeywords = 0; // tracker for correctly answered keywords
 
-  // for each keyword in keywordsArray
-  for (var iKeywords = 0; iKeywords < countKeywords; iKeywords++) {
-    // if there is a match found in userAnswerString
-    var isMatch = userAnswerString.match(keywordsArray[iKeywords]);
-    // if match() returned null, then no match in userAnswerString was found for that keyword
-    if (isMatch != null) {
-      // add 1 to matchedKeywords
-      matchedKeywords++;
-    }
-  }
+	// for each keyword in keywordsArray
+	for (var iKeywords = 0; iKeywords < countKeywords; iKeywords++) {
+		// if there is a match found in userAnswerString
+		var isMatch = userAnswerString.match(keywordsArray[iKeywords]);
+		// if match() returned null, then no match in userAnswerString was found for that keyword
+		if (isMatch != null) {
+			// add 1 to matchedKeywords
+			matchedKeywords++;
+		}
+	}
 
-  var matchPercentage = matchedKeywords / countKeywords; // % correct by the user
+	var matchPercentage = matchedKeywords / countKeywords; // % correct by the user
 
-  // if matchPercentage satisfies the threshold for a correct answer
-  if (matchPercentage >= thresholdToBeCorrect) {
-    // Then answer is correct;
-    return true;
-  } else {
-    // Then answer is incorrect;
-    return false;
-  }
+	// if matchPercentage satisfies the threshold for a correct answer
+	if (matchPercentage >= thresholdToBeCorrect) {
+		// Then answer is correct;
+		return true;
+	} else {
+		// Then answer is incorrect;
+		return false;
+	}
 }
