04ac64b33d4abc90e8231f037a7dae3b9d373139
Dragon Lore <tate.j.foster@gmail.com>
2022-08-18 10:16:12 -0600

    worked on js with some minor html and css tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var index = 0;
 var start = document.querySelector("#startBtn");
 var answerForm = document.querySelector("#answerForm");
 var answer = document.querySelector("#answer");
+var oldDrink = document.querySelector("#oldName");
+var oldGlass = document.querySelector("#oldGlass");
+var oldDrinkImg = document.querySelector("#oldDrinkImg");
+var categoriesEl = document.querySelector("#category");
+var questionsEl = document.querySelector("#question");
+var quizEl = document.querySelector("#quizBoard");
 
 function getData(data) {
 	console.log(data);
@@ -50,13 +56,13 @@ function startQuiz(event) {
 		})
 		.then(function (data) {
 			console.log(data);
+			console.log(data.drinks);
+			console.log(data.drinks[0].strDrink);
 		});
 }
 
-// TODO: FUnction to display drink recipe on loss
-
 // TODO: have event listener for click for starting game
-start.addEventListener("click", startQuiz);
+start.addEventListener("click", gameLost);
 // TODO: have event listener for submit to check answers and move on to next question
 // trim input, toLowerCase input and answer\
 // ? regular expressions to remove articles
@@ -93,5 +99,106 @@ function isCorrectAnswer(keywordsArray, userAnswerString) {
 	} else {
 		// Then answer is incorrect;
 		return false;
+	}
+}
+
+function gameLost() {
+	fetch(rewardUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			reward = data;
+			console.log(data);
+			gameReward(reward);
+		});
+
+	function gameReward(reward) {
+		start.classList.add("hidden");
+		answerForm.classList.add("hidden");
+		categoriesEl.textContent = reward.drinks[0].strDrink;
+		questionsEl.textContent = reward.drinks[0].strAlcoholic;
+		var glassType = document.createElement("h4");
+		glassType.textContent = reward.drinks[0].strGlass;
+		quizEl.appendChild(glassType);
+
+		var drinkImg = document.createElement("img");
+		drinkImg.src = reward.drinks[0].strDrinkThumb;
+		drinkImg.classList.add("w-50");
+		quizEl.appendChild(drinkImg);
+
+		var drinkInstructions = document.createElement("p");
+		drinkInstructions.textContent = reward.drinks[0].strInstructions;
+		quizEl.appendChild(drinkInstructions);
+
+		var ingredientList = document.createElement("ul");
+		ingredientList.classList.add("text-center");
+		quizEl.appendChild(ingredientList);
+
+		var ingredientSet = [];
+		ingredientSet.push(
+			reward.drinks[0].strMeasure1 + " " + reward.drinks[0].strIngredient1
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure2 + " " + reward.drinks[0].strIngredient2
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure3 + " " + reward.drinks[0].strIngredient3
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure4 + " " + reward.drinks[0].strIngredient4
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure5 + " " + reward.drinks[0].strIngredient5
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure6 + " " + reward.drinks[0].strIngredient6
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure7 + " " + reward.drinks[0].strIngredient7
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure8 + " " + reward.drinks[0].strIngredient8
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure9 + " " + reward.drinks[0].strIngredient9
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure10 + " " + reward.drinks[0].strIngredient10
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure11 + " " + reward.drinks[0].strIngredient11
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure12 + " " + reward.drinks[0].strIngredient12
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure13 + " " + reward.drinks[0].strIngredient13
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure14 + " " + reward.drinks[0].strIngredient14
+		);
+		ingredientSet.push(
+			reward.drinks[0].strMeasure15 + " " + reward.drinks[0].strIngredient15
+		);
+
+		console.log(ingredientSet);
+
+		for (i = 0; i < ingredientSet.length; i++) {
+			if (ingredientSet[i] === "null null") {
+				ingredientSet.splice([i], 1);
+				i--;
+			} else if (ingredientSet[i].includes("null")) {
+				notNull = ingredientSet[i].replace("null", "");
+				ingredientSet.splice([i], 1, notNull);
+				var newItem = document.createElement("li");
+				newItem.textContent = ingredientSet[i];
+				ingredientList.appendChild(newItem);
+			} else {
+				var newItem = document.createElement("li");
+				newItem.textContent = ingredientSet[i];
+				ingredientList.appendChild(newItem);
+			}
+		}
 	}
 }
